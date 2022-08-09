d6f33e7c20dab87d94f752cd92cbf2d891f58b93
Anupam Kaul <anupam.kaul@yahoo.com>
2022-08-09 14:59:51 -0700

    Add handson-ml examples

---
+++ ageron_book/Run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\nRunning ageron handson-ml 1"
+
+git clone https://github.com/anupamkaul/handson-ml.git
+
+cd handson-ml
+  git remote add upstream https://github.com/ageron/handson-ml.git
+  git remote -v
+  cat howto
+  echo -e "\nSimulation of how to start with handson-ml\n"
+cd ..
+
+
+
+
+
