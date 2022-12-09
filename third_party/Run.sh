19c3539d78e2d6941f212726fa771d22904787f2
Anupam Kaul <anupam.kaul@yahoo.com>
2022-12-09 12:06:00 -0800

    Add thirdparty folder for OSS (like openpose)

---
+++ third_party/Run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# open qn is do I post openpose elsewhere on my git?
+# then the pulldown and upstream checks make more sense
+
+# note: the papers folder is mine
+
+echo -e "Running openpose config (getting github source)...\n"
+
+git clone  https://github.com/CMU-Perceptual-Computing-Lab/openpose.git
+    cd openpose
+    git remote add upstream https://github.com/CMU-Perceptual-Computing-Lab/openpose.git
+    git remote -v
+    # git pull upstream to master (and git merge with mine for latest updates)
+
+
+
+
+
+
+
