73cfddd934a628369ac36bb19b84297e2fcfb1ef
Anupam Kaul <anupam.kaul@yahoo.com>
2022-12-10 09:01:47 -0800

    openpose : make run executable and checkout openpose master branch by default

--- third_party/Run.sh
+++ third_party/Run.sh
@@ -9,6 +9,7 @@ echo -e "Running openpose config (getting github source)...\n"
 
 git clone  https://github.com/anupamkaul/openpose.git
     cd openpose
+    git checkout master
     git remote add upstream https://github.com/CMU-Perceptual-Computing-Lab/openpose.git
     git remote -v
     # git pull upstream to master (and git merge with mine for latest updates)
