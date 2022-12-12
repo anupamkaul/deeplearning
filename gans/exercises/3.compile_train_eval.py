7c18e71ae53bce540dd883dac2024a0354f4340f
Anupam Kaul <anupam.kaul@yahoo.com>
2022-12-12 11:06:53 -0800

    Add matplotlib code to display random x_test images along with their classification
    Code compiles and runs but I don't see matplotlib renderings, need to investigate
    further with some sample matplotlib code.

--- gans/exercises/3.compile_train_eval.py
+++ gans/exercises/3.compile_train_eval.py
@@ -120,6 +120,60 @@ of an image from a CIFAR-10 dataset
 
 model.evaluate(x_test, y_test)
 
+"""
+The output frm the above method should be a list of metrics we are monitoring:
+categorical cross entropy and accuracy. 
+
+Further we can view (visualize) some of the test set predictions using 
+the predict method (and display using matplotlib)
+
+"""
+
+CLASSES = np.array(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog',
+                    'frog', 'horse', 'ship', 'truck'])
+
+preds = model.predict(x_test)  # preds is an array of shape [10000, 10]
+# (i.e. a vector of 10 class probabilities for each observation)
+
+preds_single =  CLASSES[np.argmax(preds, axis = -1)]
+actual_single = CLASSES[np.argmax(y_test, axis = -1)]
+
+# convert the array of probabilities back into a single prediction using numpy's argmax fn.
+# axis = -1 tells the fn to collapse the array over the last dimension (the classes dimension),
+# so that the shape of preds_single is then [10000, 1]
+
+# Let's view some of the images along side their labels and prediction. We should not be expecting
+# more than 50% accuracy (given the results reported):
+
+print("Display some results with matplotlib..\n")
+
+import matplotlib.pyplot as plt
+
+n_to_show = 10
+indices = np.random.choice(range(len(x_test)), n_to_show) # take some random images from test...
+
+fig = plt.figure(figsize=(15, 3))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+plt.show()
+
+for i, idx in enumerate(indices):
+    img = x_test[idx]
+    ax  = fig.add_subplot(1, n_to_show, i+1)
+    ax.axis('off')
+
+    ax.text(0.5, -0.35, 'pred = ' + str(preds_single[idx]), fontsize=10,
+            ha='center', transform=ax.transAxes)
+    ax.text(0.5, -0.7, 'act = ' + str(actual_single[idx]), fontsize=10,
+            ha='center', transform=ax.transAxes)
+
+    ax.imshow(img)
+
+plt.show()
+
+
+
+
+
 
 
 
