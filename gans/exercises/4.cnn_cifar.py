7ccc942e963d39cce13c865de5c052a56d80332c
Anupam Kaul <anupam.kaul@yahoo.com>
2022-12-27 11:09:43 -0800

    Add convolutional layers for CIFAR-10
    Model summary shows 2 conv layers flattened out to
    a Dense layer submitted to the output layer.

---
+++ gans/exercises/4.cnn_cifar.py
@@ -0,0 +1,114 @@
+'''
+
+One of the reasons the model is modest at 50% accuracy is because it doesn't
+exploit the spacial structure of the input images. We can use convolutional 
+layers (CNN).
+
+Text for this material is described from pages 46 to 51 of Generative Deep Learning
+
+Convolution is performed by multiplying the filter (fig 2.10) pixelwise with the portion
+of the image in question, and summing the result. The output is more positive when the
+portion of the image closely matches the filter and negative when it is more of the inverse.
+(This example is for a grayscale image, and filter is a simple 3 * 3 array with 1s and os
+and multiplication is pixel * pixel and result added to a + or - value)
+
+If we move the filter across the entire image, from left to right and top to bottom, recording
+the convolutional output as we go, we obtain a new array that 'picks out' a particular feature
+of the input, depending on the values of the filter (which are weights that get adjusted on every
+back-prop of a batch of each epoch of training). 
+
+A convolutional layer does exactly the above, but with Multiple filters instead of one. Fig 2.11
+shows 2 filters applied to an image (detect vertical and horizontal edges). Similarly if we are
+working with colored images then each filter would have 3 channels (hence multidimensional tensor)
+rather than 1 (each having shape 3 * 3 * 3) to match the R, G, B channels of the image.
+
+'''
+
+'''
+Prepare training data
+'''
+
+import numpy as np
+from keras.utils import to_categorical
+from keras.datasets import cifar10
+
+print("\n\nLoading (or reading cached values of) CIFAR-10 dataset:\n")
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# to ever re-install, delete $HOME/.keras/datasets...
+
+NUM_CLASSES = 10
+
+x_train = x_train.astype('float32') / 255.0
+x_test =  x_test.astype('float32') / 255.0
+
+y_train = to_categorical(y_train, NUM_CLASSES)
+y_test =  to_categorical(y_test, NUM_CLASSES)
+
+print("\ny_train :\n", y_train, "\n")
+print ("sample pixel in x_train : ", x_train[54, 12, 13, 1], "\n")
+
+
+'''
+Build the model
+
+In Keras the Conv2D layer applies convolutions to an input tensor with 2 spatial
+dimensions (such as an image). 
+'''
+
+from keras.layers import Input, Flatten, Dense, Conv2D
+from keras.models import Model
+
+'''
+# an example:
+
+input_layer = Input(shape=(64, 64, 1))
+
+
+# format : new_layer_type = layer_name(parameters) (old_layer_name)
+conv_layer_1 = Conv2D(
+
+               filters = 2,
+               
+               kernel_size = (3,3),
+               strides = 1,
+               padding = "same"
+               )(input_layer)
+
+'''
+
+input_layer = Input(shape=(32, 32, 3))
+
+
+# format : new_layer_type = layer_name(parameters) (old_layer_name)
+conv_layer_1 = Conv2D(
+
+               filters = 10,
+               
+               kernel_size = (4,4),
+               strides = 2,
+               padding = "same"
+               )(input_layer)
+
+conv_layer_2 = Conv2D(
+
+               filters = 20,
+               
+               kernel_size = (3,3),
+               strides = 2,
+               padding = "same"
+               )(conv_layer_1)
+
+
+flatten_layer = Flatten()(conv_layer_2)
+
+output_layer = Dense(
+               units = 10,
+               activation = "softmax"
+               ) (flatten_layer)
+
+model = Model(input_layer, output_layer)
+model.summary()
+
+
+
