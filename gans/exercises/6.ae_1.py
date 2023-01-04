d26a2e36d030b17e7d289343234a96b1192100b8
Anupam Kaul <anupam.kaul@yahoo.com>
2023-01-03 19:35:48 -0800

    Add paper Deep Feature Consistent VAE and template vae code

---
+++ gans/exercises/6.ae_1.py
@@ -0,0 +1,23 @@
+'''
+
+Exploring variational auto-encoders
+(https://arxiv.org/pdf/1312.6114.pdf)
+
+Let's first build a standard autoencoder and then
+a variational ae. VAEs are fundamental to building
+GANs. I will also highlight / pick apart both
+AEs and VAEs to understand how to build & manipulate
+autoencoder models an in particular VAEs from scratch
+to generate images (GANs) from my own training set.
+
+'''
+
+'''
+
+The first example follows from the N.Coder and D.Coder 
+actors described in pages 61 - 66 in Generative Deep Learning
+
+'''
+
+
+
